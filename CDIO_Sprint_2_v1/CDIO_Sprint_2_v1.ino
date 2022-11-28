b5c3d0508a2451060ca946dfb8a6e366ab9ca176
Heur-a <114075339+Heur-a@users.noreply.github.com>
2022-11-28 17:48:16 +0100

    Amb Funcions

--- CDIO_Sprint_2_v1/CDIO_Sprint_2_v1.ino
+++ CDIO_Sprint_2_v1/CDIO_Sprint_2_v1.ino
@@ -105,67 +105,8 @@ void pH (void) {
 
 }
 void loop() {
-  
-int16_t adc1;
-  int16_t humedad;
-  adc1 = ads1115.readADC_SingleEnded(0);
-  humedad = 100 * AirValue / (AirValue - WaterValue) - adc1 * 100 / (AirValue - WaterValue);
+ pH();
+ salinidad();
+ humedad();
 
-
-  /*Serial.print("AIN0: ");
-    Serial.println(adc1);
-    Serial.print("HR (%): ");
-    Serial.print(humedad);
-    Serial.println("%");*/
-int16_t adc0;
-   //Alimentamos sonda con tren de pulsos
-  digitalWrite(power_pin, HIGH);
-  delay (100);
-  static unsigned long samplingTime=millis();
-  static unsigned long printTime=millis();
-
-  //leemos cuando hay nivel alto
-  adc0 = analogRead(A0);
-  digitalWrite(power_pin, LOW);
-  delay(100);
-  if (adc0 < 573) {
-    adc0 = 573;
-  }
-  float ValSal = 0;
-  ValSal = 0.0000007 * adc0 * adc0 * adc0 - 0.0006 * adc0 * adc0 + 0.114 * adc0;
-
-  //Imprimimos info
-  if(millis()- printTime>printInterval) {
-    //Serial.print("Lectura digital sal = "); Serial.println(adc0, DEC);
-    if (adc0  == 573) {
-      ValSal = 0;
-    }
-    Serial.print("Cantidad de sal g= "); Serial.println(ValSal, DEC);
-  }
-
-
-  counter = counter + 1;
-
- int pHArray[ArrayLength];
-  int pHArrayIndex=0;
-  static float pHValue, voltage;
-  if(millis() - samplingTime > samplingInterval){
-    pHArray[pHArrayIndex++]= ads1115.readADC_SingleEnded(1);
-    if(pHArrayIndex== ArrayLength)pHArrayIndex = 0;
-
-    //Convertir la lectura en tension
-    voltage=(4.096/32767.0) * ads1115.readADC_SingleEnded(1);    
-    pHValue=3.5*voltage+Offset;
-    samplingTime=millis();
-  }
-  if(millis()- printTime>printInterval){
-    Serial.print("Voltage:");
-    Serial.print(voltage,2);
-    Serial.print("  pH value: ");
-    Serial.println(pHValue,2);
-    printTime=millis();
-  }
-
-
-  
 }
